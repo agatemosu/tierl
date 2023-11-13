dc7d15e806d972e4c4dc5c1747f05222c258dd71
Ana <agaatem@outlook.com>
2023-11-13 19:07:12 +0100

    WIP delete

--- script.js
+++ script.js
@@ -16,26 +16,41 @@ function changeColor(element) {
 
 function addRow() {
     const mainContainer = document.querySelector("main");
-
     const newRow = document.createElement("div");
     newRow.className = "row";
 
+
     const tierNameDiv = document.createElement("div");
     tierNameDiv.className = "tier-name";
+
     const paragraph = document.createElement("p");
-    paragraph.setAttribute("contenteditable", "");
+    paragraph.setAttribute("contenteditable", true);
     paragraph.textContent = "New tier";
     tierNameDiv.appendChild(paragraph);
+
 
     const darkRowDiv = document.createElement("div");
     darkRowDiv.className = "dark-row";
 
+
     const settingsDiv = document.createElement("div");
     settingsDiv.className = "tier-settings";
+
+    const deleteButton = document.createElement("button");
+    deleteButton.className = "delete-button";
+    deleteButton.textContent = "Delete";
+    deleteButton.onclick = function () {
+        deleteRow(this);
+    };
+    settingsDiv.appendChild(deleteButton);
+
 
     newRow.appendChild(tierNameDiv);
     newRow.appendChild(darkRowDiv);
     newRow.appendChild(settingsDiv);
-
     mainContainer.appendChild(newRow);
+}
+
+function deleteRow(element) {
+    element.parentNode.parentNode.remove();
 }
