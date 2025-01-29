410ee6869619c46e52d7e8c002044325cdb8e236
agatemosu <agaatem@outlook.com>
2025-01-29 14:11:06 +0100

    Use an image element instead of styling the background

--- src/tier-element.js
+++ src/tier-element.js
@@ -4,12 +4,9 @@ export default class TierElement extends HTMLElement {
 	 */
 	setBlob = async (blob) => {
 		const image = new Image();
-		image.onload = () => {
-			this.style.aspectRatio = `${image.width} / ${image.height}`;
-			this.style.backgroundImage = `url("${image.src}")`;
-			this.style.minHeight = `${Math.min(image.height, 80)}px`;
-		};
 		image.src = URL.createObjectURL(blob);
+
+		this.appendChild(image);
 	};
 }
 
